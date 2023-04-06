feaa3ef14702528926b6e9ae9bea2b0d452e1b96
Saadkc <saadalikc02@gmail.com>
2023-04-06 06:38:55 -0700

    commit

--- frontend/src/components/Users/addUser.js
+++ frontend/src/components/Users/addUser.js
@@ -5,37 +5,41 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import LoadingSpinner from "../common/loading_spinner";
 
+
+
 const InputValidation = ({ value, name, label, type, required, minLength, maxLength, pattern, onChange }) => {
-  const [errorMessage, setErrorMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
-  const handleInputChange = (event) => {
-    onChange(event);
-    validateInput(event.target.value);
-  };
-  const validateInput = (value) => {
-    if (required && !value) {
-      setErrorMessage(`${label} is required.`);
-    } else if (type === "email" && !/\S+@\S+\.\S+/.test(value)) {
-      setErrorMessage(`Invalid email address.`);
-    } else if (minLength && value.length < minLength) {
-      setErrorMessage(`${label} must be at least ${minLength} characters.`);
-    } else if (maxLength && value.length > maxLength) {
-      setErrorMessage(`${label} must be no more than ${maxLength} characters.`);
-    } else if (pattern && !value.match(pattern)) {
-      setErrorMessage(`${label} is invalid.`);
-    } else {
-      setErrorMessage("");
-    }
-  };
-  
 
-  return (
-    <>
-      <label htmlFor={name}>{label}:</label><br />
-      <input type={type} value={value} id={name} name={name} onChange={handleInputChange} /><br />
-      {errorMessage && <span className="error-message">{errorMessage}</span>}
-    </>
-  );
+    const handleInputChange = (event) => {
+        onChange(event);
+        validateInput(event.target.value);
+    };
+    const validateInput = (value) => {
+        if (required && !value) {
+            setErrorMessage(`${label} is required.`);
+        } else if (type === "email" && !/\S+@\S+\.\S+/.test(value)) {
+            setErrorMessage(`Invalid email address.`);
+        } else if (minLength && value.length < minLength) {
+            setErrorMessage(`${label} must be at least ${minLength} characters.`);
+        } else if (maxLength && value.length > maxLength) {
+            setErrorMessage(`${label} must be no more than ${maxLength} characters.`);
+        } else if (pattern && !value.match(pattern)) {
+            setErrorMessage(`${label} is invalid.`);
+        } else {
+            setErrorMessage("");
+
+        }
+    };
+
+
+    return (
+        <>
+            <label htmlFor={name}>{label}:</label><br />
+            <input type={type} value={value} id={name} name={name} onChange={handleInputChange} /><br />
+            {errorMessage && <span className="error-message">{errorMessage}</span>}
+        </>
+    );
 };
 
 const AddUser = () => {
@@ -46,15 +50,16 @@ const AddUser = () => {
     const [password, setPassword] = useState("");
     const [isLoading, setIsLoading] = useState(false);
 
+
     const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log("username", username);
-        if (username === "" || username === null) {
-        
+
+        if (username === "" || username.length < 4) {
+
             toast.error("Username Required", { autoClose: 2000, });
             return;
         } else {
-            if (email === "") {
+            if (email === "" || !/\S+@\S+\.\S+/.test(email)) {
                 toast.error("Email Required", { autoClose: 2000, });
                 return;
             } else {
@@ -83,22 +88,24 @@ const AddUser = () => {
         <div className="add-user">
             <form >
                 <InputValidation
-                  name="username"
-                  label="Username"
-                  type="text"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  required
-                  minLength={3}
-                  maxLength={20}
+                    name="username"
+                    label="Username"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    minLength={3}
+                    maxLength={20}
+
                 />
                 <InputValidation
-                  name="email"
-                  label="Email"
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
+                    name="email"
+                    label="Email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+
                 />
                 <label htmlFor="role">Select role:</label>
                 <select value={roleId} id="role" name="role" onChange={(e) => setRoleId(e.target.value)}>
@@ -106,14 +113,15 @@ const AddUser = () => {
                     <option value="2">User</option>
                 </select><br />
                 <InputValidation
-                  name="password"
-                  label="Password"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  minLength={6}
-                  pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    minLength={6}
+                    pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$"
+
                 />
             </form>
             <button className="button" onClick={handleSubmit} disabled={isLoading}> {isLoading ? <LoadingSpinner /> : "Submit"}</button >
